dc40d0585cbbd816370e58c8bef1011517611e47
Daniel Reinert <daniel.reinert@dwd.de>
2022-09-25 17:35:14 +0000

    Buildbot: Fix FALSE positives for NEC builder (some additional debug output is kept for the time being)
    See merge request icon/icon-nwp!586

--- scripts/buildbot_scripts/pbs_job.py
+++ scripts/buildbot_scripts/pbs_job.py
@@ -18,17 +18,52 @@ class PBSJob(BatchJob):
 
         submit_cmd.append(script)
         print("submitting PBS job: '{}'".format(" ".join(submit_cmd)))
-        qsub = subprocess.Popen(submit_cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.cwd, encoding="UTF-8")
-        
+        qsub = subprocess.Popen(submit_cmd,
+                                shell=False,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                cwd=self.cwd,
+                                encoding="UTF-8")
+
         try:
-            self.jobid = re.findall(r"\d{5,100}.\w{3,100}", qsub.stdout.readline())[0]
+            stdout = qsub.stdout.readlines()[0]
+            print('|qsub: stdout = {}|'.format(stdout))
+            self.jobid = re.findall(r"(\d+)\.\w+", stdout)[0]
+            print("|qsub: jobId = {}|".format(self.jobid))
         except:
             for line in qsub.stderr.readlines():
-                print(line)
+                print("|qsub STDERR: {}|".format(line))
 
         if not self.jobid:
             print("Parsing jobid from pbs job failed, got {}".format(self.jobid))
             sys.exit(1)
 
         # qsub will return immediately. That's why we pass along qwait
-        self.job = subprocess.Popen(["qwait", self.jobid])
+        qwaitCmd = 'qwait {}'.format(self.jobid)
+        print('|qwait call: "{}"|'.format(qwaitCmd))
+        self.job = subprocess.Popen(qwaitCmd,
+                                    shell=True,
+                                    stderr=subprocess.PIPE,
+                                    stdout=subprocess.PIPE,
+                                    encoding="utf-8")
+
+    def wait(self):
+        # PBS does not return the exitcode of the submitted script. instead it prints it to stdout
+        ret = self.job.communicate()
+        stdout = ret[0]
+        stderr = ret[1]
+
+        print('|qwait:stdout: {}|'.format(stdout))
+        print('|qwait:stderr: {}|'.format(stderr))
+
+        try:
+            exit_code = int(stderr.split()[-1])
+            print('qwait: exit_code = |{}|'.format(exit_code))
+        except:
+            print('pbs_job.py: Could not get a proper return value from "qwait"')
+            print('qwait: stderr = |{}|'.format(stderr))
+            exit_code = 1
+
+        self.returncode = exit_code
+
+        return exit_code
