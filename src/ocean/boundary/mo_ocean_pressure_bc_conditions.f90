73b6c05f36d3931ad4299f74b9da880d528a39c2
Helmuth Haak <helmuth.haak@mpimet.mpg.de>
2022-11-24 09:59:18 +0000

    [icon-mpim:bugfix-oes-zstar-bctop] bugfix: air pressure was not considered as top boundary condition in zstar
    https://gitlab.dkrz.de/icon/icon-mpim/-/merge_requests/2
    
    bugfix: air pressure is now considered as top bc in the case of zstar (see issue #2)
    
    Co-authored-by: Helmuth Haak <m211054@levante1.atos.local>
    Co-authored-by: Helmuth Haak <m211054@levante5.atos.local>
    Co-authored-by: Helmuth Haak <m211054@levante3.atos.local>
    Co-authored-by: Helmuth Haak <m211054@levante2.atos.local>
    Co-authored-by: Helmuth Haak <m211054@levante6.atos.local>
    Co-authored-by: Helmuth Haak <m211054@breeze2.mpimet.mpg.de>
    Co-authored-by: Helmuth Haak <m211054@breeze1.mpimet.mpg.de>
    Co-authored-by: Helmuth Haak <m211054@l40357.atos.local>
    Approved-by: Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
    Merged-by: Helmuth Haak <helmuth.haak@mpimet.mpg.de>
    
    Squashed commit of the following:
    
    * [icon-oes:bugfix_zstar-bc-top-potential] refactor to do loop
    
    * [icon-oes:bugfix_zstar-bc-top-potential] refactor caculation of bc_total_top_potential
    
    * [icon-oes:bugfix_zstar-bc-top-potential] fix typo
    
    * [icon-oes:bugfix_zstar-bc-top-potential] bugfix: air pressure was not condidered as top bc in the case of zstar

--- src/ocean/boundary/mo_ocean_pressure_bc_conditions.f90
+++ src/ocean/boundary/mo_ocean_pressure_bc_conditions.f90
@@ -15,21 +15,21 @@ MODULE mo_ocean_pressure_bc_conditions
   !-------------------------------------------------------------------------
   USE mo_kind,                   ONLY: wp, dp
    USE mtime,                    ONLY: datetime
-  USE mo_exception,              ONLY: message, finish, warning, message_text
-  USE mo_model_domain,           ONLY: t_patch, t_patch_3d
-  USE mo_ocean_nml,              ONLY: n_zlev, use_tides,   &
+!  USE mo_exception,              ONLY: message, finish, warning, message_text
+  USE mo_model_domain,           ONLY: t_patch_3d
+  USE mo_ocean_nml,              ONLY: use_tides,   &
     & use_tides_SAL, atm_pressure_included_in_ocedyn,       &
-    & OceanReferenceDensity_inv
+    & OceanReferenceDensity_inv, vert_cor_type
   USE mo_physical_constants,     ONLY: grav
-  USE mo_grid_subset,            ONLY: t_subset_range, get_index_range
-  USE mo_parallel_config,        ONLY: nproma
+ ! USE mo_grid_subset,            ONLY: t_subset_range, get_index_range
+ ! USE mo_parallel_config,        ONLY: nproma
   USE mo_impl_constants,         ONLY: sea_boundary
-  USE mo_ocean_math_operators,   ONLY: grad_fd_norm_oce_2d_3d
   USE mo_util_dbg_prnt,          ONLY: dbg_print, debug_printValue
-  USE mo_ocean_tides,            ONLY: calculate_tides_potential  
+  USE mo_ocean_tides,            ONLY: calculate_tides_potential
   USE mo_ocean_surface_types,    ONLY: t_atmos_for_ocean
   USE mo_ocean_types,            ONLY: t_hydro_ocean_state
-  USE mo_dynamics_config,        ONLY: nold, nnew
+  USE mo_sea_ice_types,          ONLY: t_sea_ice
+  USE mo_dynamics_config,        ONLY: nold
 
 
   IMPLICIT NONE
@@ -37,43 +37,73 @@ MODULE mo_ocean_pressure_bc_conditions
   PRIVATE
 
   PUBLIC  :: create_pressure_bc_conditions
-  
+
   CHARACTER(LEN=12)  :: str_module = 'mo_ocean_pressure_bc_conditions'  ! Output of module for 1 line debug
   !-------------------------------------------------------------------------
-  
+
 CONTAINS
 
   !-------------------------------------------------------------------------
-  SUBROUTINE create_pressure_bc_conditions(patch_3d,ocean_state, p_as,current_time)
+  SUBROUTINE create_pressure_bc_conditions(patch_3d,ocean_state, p_as,sea_ice, current_time)
     TYPE(t_patch_3d ),TARGET, INTENT(in)             :: patch_3d
     TYPE(t_hydro_ocean_state), TARGET, INTENT(inout) :: ocean_state
-    TYPE(t_atmos_for_ocean)                          :: p_as    
-    TYPE(datetime), POINTER                          :: current_time
-      
+    TYPE(t_atmos_for_ocean), TARGET, INTENT(in)      :: p_as
+    TYPE (t_sea_ice), TARGET, INTENT(in)             :: sea_ice
+    TYPE(datetime), POINTER, INTENT(in)              :: current_time
+
+    REAL(wp) :: switch_atm_pressure, switch_vert_cor_type, switch_tides
+
     !------------------------------------------------------------------------
-    ! compute tidal potential
-    CALL calculate_tides_potential(patch_3d,current_time,ocean_state%p_diag%rho, ocean_state%p_prog(nold(1))%h, &
-      ocean_state%p_aux%bc_tides_potential, ocean_state%p_aux%bc_SAL_potential) 
+    IF (use_tides .OR. use_tides_SAL) THEN
+      ! compute tidal potential
+      CALL calculate_tides_potential(patch_3d,current_time,ocean_state%p_diag%rho, ocean_state%p_prog(nold(1))%h, &
+           ocean_state%p_aux%bc_tides_potential, ocean_state%p_aux%bc_SAL_potential)
+    ENDIF
+
+    !------------------------------------------------------------------------
+    ! total top potential (individiual terms)
+
+    IF (atm_pressure_included_in_ocedyn) THEN
+      switch_atm_pressure=1.0_wp
+    ELSE
+      switch_atm_pressure=0.0_wp
+    ENDIF
+
+    IF (vert_cor_type .EQ. 1 ) THEN
+      switch_vert_cor_type=1.0_wp
+    ELSE
+      switch_vert_cor_type=0.0_wp
+    ENDIF
+
+    IF (use_tides .OR. use_tides_SAL) THEN
+      switch_tides=1.0_wp
+    ELSE
+      switch_tides=0.0_wp
+    ENDIF
+
     !------------------------------------------------------------------------
     ! total top potential
-    IF (atm_pressure_included_in_ocedyn) THEN
+
+    IF ( (switch_atm_pressure + switch_vert_cor_type + switch_tides) > 0.0_wp ) THEN
+
       ocean_state%p_aux%bc_total_top_potential =  &
         & ocean_state%p_aux%bc_tides_potential &
         & + ocean_state%p_aux%bc_SAL_potential &
-        & + p_as%pao * OceanReferenceDensity_inv
-    
-    ELSE IF (use_tides .or. use_tides_SAL) THEN
-      ocean_state%p_aux%bc_total_top_potential =  ocean_state%p_aux%bc_tides_potential + ocean_state%p_aux%bc_SAL_potential
+        & + p_as%pao * OceanReferenceDensity_inv * switch_atm_pressure & ! add acceleration by air pressure
+        & + grav * sea_ice%draftave * switch_vert_cor_type ! only zstar: pressure of sea ice on top of the first layer (divided by rhoref to create an acceleration)
+
     ENDIF
-                            
-    CALL dbg_print('tides_potential',  ocean_state%p_aux%bc_tides_potential, &
-      str_module, 3, in_subset=patch_3d%p_patch_2d(1)%cells%owned)
-    CALL dbg_print('tides_SAL',      ocean_state%p_aux%bc_SAL_potential, &
-      str_module, 3, in_subset=patch_3d%p_patch_2d(1)%cells%owned)
-        
+
+     IF (use_tides .OR. use_tides_SAL) THEN
+      CALL dbg_print('tides_potential',  ocean_state%p_aux%bc_tides_potential, &
+           str_module, 3, in_subset=patch_3d%p_patch_2d(1)%cells%owned)
+      CALL dbg_print('tides_SAL',      ocean_state%p_aux%bc_SAL_potential, &
+           str_module, 3, in_subset=patch_3d%p_patch_2d(1)%cells%owned)
+    ENDIF
+
   END SUBROUTINE create_pressure_bc_conditions
   !-------------------------------------------------------------------------
-  
+
 
 END MODULE mo_ocean_pressure_bc_conditions
 !=============================================================================
