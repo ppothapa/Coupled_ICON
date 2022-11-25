d7d56ef560589a52a44bc55ff6fbb87d16e834ae
Daniel Reinert <daniel.reinert@dwd.de>
2022-11-25 12:43:01 +0000

    fix non-ASCII characters

--- src/atm_phy_schemes/mo_turb_vdiff_params.f90
+++ src/atm_phy_schemes/mo_turb_vdiff_params.f90
@@ -10,11 +10,11 @@
 !!
 !! References:
 !!     Angevine, W. M., Jiang, H., & Mauritsen T. (2010).
-!!           Performance of an eddy diffusivity- mass flux scheme for shallow cumulus boundary layers.
-!!           Monthly Weather Review, 138(7), 2895–2912. https://doi.org/10.1175/2010MWR3142.1
+!!           Performance of an eddy diffusivity mass flux scheme for shallow cumulus boundary layers.
+!!           Monthly Weather Review, 138(7), 2895-2912. https://doi.org/10.1175/2010MWR3142.1
 !!     Mauritsen, T., & Svensson, G. (2007).
 !!           Observations of stably stratified shear-driven atmospheric turbulence at low and high Richardson numbers.
-!!           Journal of the Atmospheric Sciences, 64(2), 645–655. https://doi.org/10.1175/JAS3856.1
+!!           Journal of the Atmospheric Sciences, 64(2), 645-655. https://doi.org/10.1175/JAS3856.1
 !!
 !! @par Copyright and License
 !!
