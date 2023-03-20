8792df95ff07b361455bf7b5598f92d38b1764ce
Veronika Gayler <veronika.gayler@mpimet.mpg.de>
2023-03-20 08:14:44 +0000

    [mpim] ICON-Land update for Land offline simulations
    https://gitlab.dkrz.de/icon/icon-mpim/-/merge_requests/83
    
    Update of ICON-Land to enable ICON-Land offline simulations. The update is based on
    cherry-picks from icon-mpim:land-dev. This comprises
    - adapters for orbit and solar calculations in mo_util_jsbach
    - further fixes in mo_util_jsbach
    - a new runscript to test ICON-Land standalone simulations
    - and a new jsbach external.
    
    Co-authored-by: Reiner Schnur <reiner.schnur@mpimet.mpg.de>
    Merged-by: Veronika Gayler <veronika.gayler@mpimet.mpg.de>

--- src/atm_phy_aes/mo_aes_thermo.f90
+++ src/atm_phy_aes/mo_aes_thermo.f90
@@ -109,7 +109,7 @@ SUBROUTINE saturation_adjustment ( ilo,  iup,  klo,  kup, &
 !!!=============================================================================================
 
   !$ACC PARALLEL DEFAULT(PRESENT)
-  !$ACC LOOP GANG VECTOR TILE(128,1)
+  !$ACC LOOP GANG VECTOR TILE(128, 1)
   DO k = klo, kup
     DO i = ilo , iup
       qt      = qve(i,k) + qce(i,k) + qre(i,k) + qti(i,k)
